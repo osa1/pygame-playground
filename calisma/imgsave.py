62ce2800267c05c6f8cb7add4b0512beb9188f37
Ömer Sinan Ağacan <omeragacan@gmail.com>
2011-12-06 15:51:01 +0200

    cleanup

--- calisma/imgsave.py
+++
@@ -1,7 +0,0 @@
-import pygame
-pygame.init()
-
-text = "Omer Sinan Agacan"
-font = pygame.font.SysFont("arial", 64)
-surface = font.render(text, True, (0,0,0), (255,255,255))
-pygame.image.save(surface, "name.png")
